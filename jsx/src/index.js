4890879e16893f8dfc12f8c7c728968e21a00e23
Riley-Robinson <rrtoinfinity@gmail.com>
2021-01-27 14:32:38 -0500

    new push not alot accomplished

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div> hellow world!</div>;
+
+    const butttonText = 'Click Me!';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name"> Enter Name: </label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+               {butttonText} </button>
+        </div>
+    );
 };
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+ 
